251b34fe6bc3e28d83a33fe8275994b27c55e85c
danbons <daniele.bonini1988@gmail.com>
2026-04-25 08:39:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "15mb" }));
+app.use(cors());
+app.use(express.json({ limit: "15mb" }));
+
+app.get("/", (_req, res) => {
+  res.send("Refleksa backend OK");
+});
 
 // ===============================
 // BASIC API LOGGING
