e6be8000ebe9aa2bf82c36cb68d9e78cd113215c
danbons <daniele.bonini1988@gmail.com>
2026-04-27 10:15:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ const prototypeDevices = [
     expiresAt: "2026-05-20T23:59:59Z"
   },
   {
+    deviceId: "cf11c48103db426b",
+    partner: "eric-chenhang",
+    enabled: true,
+    expiresAt: "2026-05-20T23:59:59Z"
+  },
+  {
     deviceId: "f5f802377bd3383c",
     partner: "daniele-tablet",
     enabled: true,
