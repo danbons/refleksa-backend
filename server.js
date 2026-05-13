bdab726e76c3231d62c42ce95fd56908a2479e04
danbons <daniele.bonini1988@gmail.com>
2026-05-13 08:59:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -392,7 +392,7 @@ app.post("/session", requirePrototypeToken, async (req, res) => {
         session: {
           type: "realtime",
           model: "gpt-realtime",
-          output_modalities: ["text"],
+          output_modalities: ["text", "audio"],
           instructions: `
 You are Refleksa.
 
