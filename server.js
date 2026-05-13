946f27428812cfe96818373ab4a6d95b1714eb36
danbons <daniele.bonini1988@gmail.com>
2026-05-13 13:40:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -449,7 +449,7 @@ Avoid:
 If the user asks to go home or standby:
 respond briefly and stop.
 `.trim(),
-          max_output_tokens: 900
+          max_output_tokens: 1200
         }
       })
     });
